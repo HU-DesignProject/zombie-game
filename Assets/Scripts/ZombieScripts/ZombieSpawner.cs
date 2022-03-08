e0bc10449016eb6f9b07915f3f3d549913833a82
ayse-balci <abalci1485@gmail.com>
2022-03-08 19:17:29 +0300

    Zombie Romero added

---
+++ Assets/Scripts/ZombieScripts/ZombieSpawner.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieSpawner : MonoBehaviour {
+
+	public float respawnDuration = 5.0f;
+	public List<GameObject> spawnPoints = new List<GameObject>();
+    public int startHealth = 100;
+	public float startMoveSpeed = 1f;
+	public float startDamage = 15f;
+	public int startEXP = 3;
+	public int startFund = 5;
+	public float upgradeDuration = 60f;	// Increase all enemy stats every 30 seconds
+
+    private float upgradeTimer;
+	[SerializeField]
+	private int currentHealth;
+	[SerializeField]
+	private float currentMoveSpeed;
+	[SerializeField]
+	private float currentDamage;
+	[SerializeField]
+	private int currentEXP;
+	[SerializeField]
+	private int currentFund;
+    private float spawnTimer;
+    public Transform target;
+    public GameObject zombie;
+	private List<GameObject> enemies = new List<GameObject>();
+
+	void Start() {
+        currentHealth = startHealth;
+		currentMoveSpeed = startMoveSpeed;
+		currentDamage = startDamage;
+		currentEXP = startEXP;
+		currentFund = startFund;
+
+        enemies.Add(zombie);
+
+    }
+
+    void Update() {
+		if(spawnTimer < respawnDuration) {
+			spawnTimer += Time.deltaTime;
+		}
+		else {
+			SpawnEnemy();
+		}
+
+		if(upgradeTimer < upgradeDuration) {
+			upgradeTimer += Time.deltaTime;
+		}
+		else {
+			UpgradeEnemy();
+		}
+	}
+
+    float GetDistanceFrom(Vector3 src, Vector3 dist) {
+		return Vector3.Distance(src, dist);
+	}
+
+    void SpawnEnemy() {
+		if(spawnTimer < respawnDuration) return;
+
+		foreach(GameObject spawnPoint in spawnPoints) {
+			GameObject zombie = enemies[0];
+			zombie.GetComponent<AIExample>().fpsc = target;
+			zombie.GetComponent<NavMeshAgent>().speed = currentMoveSpeed;
+			zombie.GetComponent<AIExample>().health = currentHealth;
+
+			// Boost rotating speed
+			float rotateSpeed = 120f + currentMoveSpeed;
+			rotateSpeed = Mathf.Max(rotateSpeed, 200f);	// Max 200f
+			zombie.GetComponent<NavMeshAgent>().angularSpeed = rotateSpeed;
+
+			// PhotonNetwork.Instantiate("Zombie", spawnPoint.transform.position, spawnPoint.transform.rotation, 0);
+			Instantiate(zombie, spawnPoint.transform.position, spawnPoint.transform.rotation);
+		}
+		
+		spawnTimer = 0f;
+	}
+
+    void UpgradeEnemy() {
+		print("ENEMY UPGRADED");
+
+		currentHealth += 5;
+
+		if(currentMoveSpeed < 4f) {
+			currentMoveSpeed += 0.2f;
+		}
+		if(currentDamage < 51f) {
+			currentDamage += 2f;
+		}
+		
+		currentEXP++;
+		currentFund++;
+
+		upgradeTimer = 0;
+	}
+
+   /* public GameObject[] zombies;
+    public GameObject zombie;
+	private void Start() {
+        zombies = new GameObject[5];
+
+        for (int i = 0; i < zombies.Length; i++ )
+        {
+            zombies[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SpawnZombie();
+        }
+    }
+
+    private void SpawnZombie()
+    {
+        int zombieID = Random.Range(0, zombies.Length);
+        Instantiate(zombie, zombies[zombieID].transform.position, zombies[zombieID].transform.rotation);
+    }
+*/
+}
